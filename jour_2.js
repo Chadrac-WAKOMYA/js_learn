0ffd2ab41b541c3117d5816702a92a1b25a370fe
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-11-25 16:01:00 +0200

    array exercise done

--- jour_2.js
+++ jour_2.js
@@ -10,3 +10,12 @@ const livre = {
         return this.title + "  " + this.author
     }
 };
+
+nombre = [1,2,4,5,8,3,9];
+function calMoyen(tabNbre){
+    var sum = 0;
+    tabNbre.forEach(nbre => {
+        sum += nbre;
+    });
+    return sum/tabNbre.length;
+}
