0aefcb5312573cf1e15a6718f9c097049c300884
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-11-25 15:49:32 +0200

    objet added

--- jour_2.js
+++ jour_2.js
@@ -2,3 +2,11 @@ function fonct(nbre){
     return null;
 }
 
+
+const livre = {
+    title : "the truth",
+    author : "Mr Chadrac",
+    makeSummary : function (){
+        return this.title + "  " + this.author
+    }
+};
