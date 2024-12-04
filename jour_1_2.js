5c93e6d8677d61b9aec7e8c12c7a789a993578a8
KIGHUNDILA WAKOMYA Chadrac <chadracxx@gmail.com>
2024-12-04 15:46:44 +0200

    exercise datatype finished

--- jour_1_2.js
+++ jour_1_2.js
@@ -4,7 +4,7 @@ caractersVariable = "Bonjour les amis";
 numberVariable = 3;
 booleanVariable = 2>4;
 nullVariable = null;
-undefinedVariable;
+undefinedVariable = undefined;
 objectVarible = {
     firstElement : "firstElement",
     secondElement : "secondElement",
@@ -18,11 +18,13 @@ arrayVaraible = ["element1", "element2", "element3", "element4"];
 console.log( 
     'These are my variables' 
     + 'String : ' +caractersVariable + '    ' 
-    + 'Number :' + numnerVariable + '    '
+    + 'Number :' + numberVariable + '    '
     + 'Boolean : ' +booleanVariable + '    ' 
     + 'Null : ' + nullVariable + '    ' 
     + 'Undefined : ' + undefinedVariable + '    ' 
-    + 'Object : ' + objectVarible + '    '
+    + 'Object : ' + objectVarible.toString() + '    '
     + 'Array : ' + arrayVaraible
 );
+
+
 
